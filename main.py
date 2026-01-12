ff86aa7897f7ca388477113be498db4dbed8aa27
Mykhailo Moroz <moroz1456@gmail.com>
2026-01-12 15:53:01 -0500

    Stopwatch upd

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton, QHBoxLayout
 from PyQt5.QtCore import QTimer, QTime, Qt
 
-class DigitalClock(QWidget):
+class Stopwatch(QWidget):
     def __init__(self):
         super().__init__()
-        self.time_label = QLabel(self)
+        self.time = QTime(0, 0, 0, 0)
+        self.time_label = QLabel("00:00:00.00", self)
+        self.start_button = QPushButton("Start", self)
+        self.stop_button = QPushButton("Stop", self)
+        self.reset_button = QPushButton("Reset", self)
         self.timer = QTimer(self)
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle("Digital Clock")
-        self.setGeometry(400, 400, 300, 100)
+        self.setWindowTitle("Stopwatch")
 
         vbox = QVBoxLayout()
         vbox.addWidget(self.time_label)
@@ -19,24 +22,56 @@ class DigitalClock(QWidget):
 
         self.time_label.setAlignment(Qt.AlignCenter)
 
-        self.time_label.setStyleSheet("font-size:150px;"
-                                      "color:green;")
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.start_button)
+        hbox.addWidget(self.stop_button)
+        hbox.addWidget(self.reset_button)
+        vbox.addLayout(hbox)
 
-        self.setStyleSheet("background-color:black;")
+        self.setStyleSheet("""
+            QPushButton, QLabel {
+                padding: 20px;
+                font-weight: bold;
+            }
+            QPushButton {
+                font-size: 50px;
+            }
+            QLabel {
+                font-size: 120px;
+            }
+        """)
 
-        self.timer.timeout.connect(self.update_time)
-        self.timer.start(1000)
+        self.start_button.clicked.connect(self.start)
+        self.stop_button.clicked.connect(self.stop)
+        self.reset_button.clicked.connect(self.reset)
+        self.timer.timeout.connect(self.update_display)
 
-        self.update_time()
+    def start(self):
+        self.timer.start(10)
 
-    def update_time(self):
-        current_time = QTime.currentTime().toString("hh:mm:ss AP")
-        self.time_label.setText(current_time)
+    def stop(self):
+        self.timer.stop()
+
+    def reset(self):
+        self.timer.stop()
+        self.time = QTime(0, 0, 0, 0)
+        self.time_label.setText(self.format_time(self.time))
+
+    def format_time(self, time):
+        hours = time.hour()
+        minutes = time.minute()
+        seconds = time.second()
+        milliseconds = time.msec() // 10
+        return f"{hours:02}:{minutes:02}:{seconds:02}.{milliseconds:02}"
+
+    def update_display(self):
+        self.time = self.time.addMSecs(10)
+        self.time_label.setText(self.format_time(self.time))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    clock = DigitalClock()
-    clock.show()
+    stopwatch = Stopwatch()
+    stopwatch.show()
     sys.exit(app.exec_())
 
 
